01388ebd35a373760070f0ffb7caf5b0e0193d88
adeilton157 <adeiltondantas07@gmail.com>
2023-12-04 19:43:14 -0300

    first

---
+++ posts.py
@@ -0,0 +1,41 @@
+posts = [
+    {
+        'autor': 'Adeilton Barros',
+        'titulo': 'Trailer de GTA 6 teve data revelada',
+        'conteudo': 'GTA 6 terá o seu primeiro trailer revelado em menos de 24 horas. O vídeo com mais informações sobre o futuro lançamento da Rockstar Games...',
+        'data': '04/12/2023',
+        'url': 'https://www.techtudo.com.br/noticias/2023/12/trailer-de-gta-6-teve-data-revelada-em-gta-online-mas-ninguem-percebeu-edjogos.ghtml',
+    },
+
+    {
+        'autor': 'Adeilton Barros',
+        'titulo': 'The Last of Us 3 ainda não tem roteiro',
+        'conteudo': 'The Last of Us 3 ainda é o sonho de muitos fãs da franquia da Naughty Dog...',
+        'data': '04/12/2023',
+        'url': 'https://meups.com.br/noticias/the-last-of-us-3-ainda-nao-tem-roteiro-tommy/',
+    },
+
+    {
+        'autor': 'Adeilton Barros',
+        'titulo': 'Atriz de “Orange is the New Black” entra para elenco de “Minecraft”',
+        'conteudo': 'A atriz Danielle Brooks contracenará com Jason Momoa, que será o protagonista do filme baseado no jogo de computador...',
+        'data': '01/12/2023',
+        'url': 'https://www.cnnbrasil.com.br/entretenimento/atriz-de-orange-is-the-new-black-entra-para-elenco-de-minecraft/',
+    },
+
+    {
+        'autor': 'Adeilton Barros',
+        'titulo': 'Genesis X Gran Berlinetta Vision Gran Turismo Concept é revelado',
+        'conteudo': 'O Genesis X Gran Berlinetta Vision Gran Turismo Concept aplica emocionalmente a filosofia de design Athletic Elegance do Genesis...',
+        'data': '02/12/2023',
+        'url': 'https://psxbrasil.com.br/genesis-x-gran-berlinetta-vision-gran-turismo-concept-e-revelado/',
+    },
+
+    {
+        'autor': 'Adeilton Barros',
+        'titulo': 'GOTY 2023: conheça os indicados ao prêmio de “Melhor Jogo do Ano”',
+        'conteudo': 'Grande ano para os games, 2023 traz uma verdadeira disputa de gigantes para o "Oscar dos jogos"',
+        'data': '13/11/2023',
+        'url': 'https://meups.com.br/noticias/goty-2023-indicados-premio-melhor-jogo-ano/',
+    }
+]
